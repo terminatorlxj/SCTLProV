a4a06aa8b05a6984d3a4119c108d30385bdc7e52
Jian <terminatorlxj@163.com>
2017-03-31 10:51:07 +0800

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
-linux: term.ml formula.ml modul.ml prover.ml prover_output.ml lexer.mll parser.mly main.ml
+linux: bdd.ml term.ml formula.ml modul.ml prover.ml prover_bdd.ml prover_output.ml lexer.mll parser.mly main.ml
 	ocamllex lexer.mll       
 	ocamlyacc parser.mly 
-	ocamlopt -o sctl term.ml formula.ml modul.ml prover.ml prover_output.ml parser.mli parser.ml lexer.ml main.ml 
+	ocamlfind ocamlopt -o sctl -package cudd -linkpkg -g bdd.ml term.ml formula.ml modul.ml prover.ml prover_bdd.ml prover_output.ml parser.mli parser.ml lexer.ml main.ml
 
-win: term.ml formula.ml modul.ml prover.ml prover_output.ml lexer.mll parser.mly main.ml
+win: bdd.ml term.ml formula.ml modul.ml prover.ml prover_bdd.ml prover_output.ml lexer.mll parser.mly main.ml
 	ocamllex lexer.mll       
 	ocamlyacc parser.mly 
-	ocamlopt -o sctl.exe term.ml formula.ml modul.ml prover.ml prover_output.ml parser.mli parser.ml lexer.ml main.ml 
+	ocamlfind ocamlopt -o sctl.exe -package cudd -linkpkg -g bdd.ml term.ml formula.ml modul.ml prover.ml prover_bdd.ml prover_output.ml parser.mli parser.ml lexer.ml main.ml
 
 clean:
 	rm -f *.cm[ioax]
--- main.ml
+++ main.ml
@@ -1,4 +1,3 @@
-
 open Term 
 open Formula
 open Modul
@@ -9,45 +8,45 @@ open Parser
 let input_paras pl = 
 	let rec get_para_from_stdin i paras = 
 		match paras with
-		| (v, vt) :: paras' -> (match vt with
-								| Int_type (i1, i2) -> (Const (int_of_string Sys.argv.(i)))
-								| Bool_type -> (Const (let b = Sys.argv.(i) in (if b="true" then 1 else (if b="false" then (-1) else 0))))
-								| _ -> print_endline ("invalid input for parameter: "^v^"."); exit 1) :: (get_para_from_stdin (i+1) paras')
+		| (v, vt) :: paras' -> 
+			(match vt with
+			| Int_type (i1, i2) -> (Const (int_of_string Sys.argv.(i)))
+			| Bool_type -> (Const (let b = Sys.argv.(i) in (if b="true" then 1 else (if b="false" then (0) else (-1)))))
+			| _ -> print_endline ("invalid input for parameter: "^v^"."); exit 1) :: (get_para_from_stdin (i+1) paras')
 		| [] -> [] in
 	get_para_from_stdin 2 pl
 
-let main () = 
-	let arg_length = Array.length Sys.argv in
-	if arg_length = 2 then 
-	begin
-		try
-			let (modl_tbl, modl) = Parser.input Lexer.token (Lexing.from_channel (open_in Sys.argv.(1))) in
-			let modl_tbl1 = Hashtbl.create (Hashtbl.length modl_tbl) 
-			and modl1 = modul021 modl in	
-			Hashtbl.iter (fun a b -> Hashtbl.add modl_tbl1 a (modul021 b)) modl_tbl;
-			let modl2 = modul122 modl1 (input_paras modl1.parameter_list) modl_tbl1 in
-			let modl3 = modul223 modl2 in
-			let modl4 = modul324 modl3 in
-			let modl5 = modul425 modl4 in
-			Prover.prove_model modl5
-		with Parsing.Parse_error -> print_endline ("parse error at line: "^(string_of_int (!(Lexer.line_num))))
-	end else 
-	(
-		try
-			let (modl_tbl, modl) = Parser.input Lexer.token (Lexing.from_channel (open_in Sys.argv.(3))) in
-			let modl_tbl1 = Hashtbl.create (Hashtbl.length modl_tbl) 
-			and modl1 = modul021 modl in	
-			Hashtbl.iter (fun a b -> Hashtbl.add modl_tbl1 a (modul021 b)) modl_tbl;
-			let modl2 = modul122 modl1 (input_paras modl1.parameter_list) modl_tbl1 in
-			let modl3 = modul223 modl2 in
-			let modl4 = modul324 modl3 in
-			let modl5 = modul425 modl4 in
-			let out = open_out Sys.argv.(2) in
-			Prover_output.Seq_Prover.prove_model modl5 out Sys.argv.(2);
-			close_out out
-	
-		with Parsing.Parse_error -> print_endline ("parse error at line: "^(string_of_int (!(Lexer.line_num))))
 
-	)
+let choose_to_prove bdd output_file input_file = 
+	try
+		let (modl_tbl, modl) = Parser.input Lexer.token (Lexing.from_channel (open_in input_file)) in
+		let modl_tbl1 = Hashtbl.create (Hashtbl.length modl_tbl) 
+		and modl1 = modul021 modl in	
+		Hashtbl.iter (fun a b -> Hashtbl.add modl_tbl1 a (modul021 b)) modl_tbl;
+		let modl2 = modul122 modl1 (input_paras modl1.parameter_list) modl_tbl1 in
+		let modl3 = modul223 modl2 in
+		let modl4 = modul324 modl3 in
+		let modl5 = modul425 modl4 in
+		match (bdd, output_file) with
+		| (true, None) -> Prover_bdd.prove_model modl5
+		| (false, None) -> Prover.prove_model modl5
+		| (_, Some filename) -> 
+			let out = open_out filename in
+			Prover_output.Seq_Prover.prove_model modl5 out filename;
+			close_out out
+	with Parsing.Parse_error -> print_endline ("parse error at line: "^(string_of_int (!(Lexer.line_num))))
+	
+
+let main () = 
+	let use_bdd = ref false 
+	and output_file = ref None in
+	Arg.parse
+		[
+			"-bdd", Arg.Unit (fun () -> use_bdd := true), " Whether using BDDs to store state sets";
+			"-output", Arg.String (fun s -> output_file := Some s), " The output file";
+		]
+		(fun s -> choose_to_prove !use_bdd !output_file s)
+		"Usage: sctl [-bdd] [-output <filename>] <filename>"
+
 let _ = 
 	Printexc.print main ()
